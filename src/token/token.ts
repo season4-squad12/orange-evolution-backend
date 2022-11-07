73f03f9cb7fd6cd5d1b7f21157bdc156bf0d0023
dhenycruz <dheniarley.ds@gmail.com>
2022-11-07 19:30:09 -0300

    refact: refatorando o arquivo token e tipando

---
+++ src/token/token.ts
@@ -0,0 +1,42 @@
+import jwt = require('jsonwebtoken');
+import User from '../database/models/user';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+
+interface IData {
+  data: {
+    id: string,
+    name: string,
+    lastName: string,
+    email: string,
+    role: string,
+  }
+}
+
+const createToken = (user: User) => {
+  const secret = process.env.SECRET_KEY;
+  if (secret) {
+    const token = jwt.sign({ data: user }, secret, { expiresIn: '7d', algorithm: 'HS256' });
+    return token;
+  }
+};
+
+const authToken = async (token: string) => {
+  const secret = process.env.SECRET_KEY;
+  try {
+    if (secret) {
+      const decodec = jwt.verify(token, secret) as IData;
+      const user = await User.findAll({ where: { email: decodec.data.email } });
+      if (!user) return false;
+      return {
+        auth: true,
+        user: decodec.data,
+      };
+    }
+  } catch (e) {
+    return false;
+  }
+};
+
+export default { createToken, authToken };
